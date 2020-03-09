2c614e7692488985ada4e0460c0d0e3f063e14f8
Jake Frank <jacob423frank@gmail.com>
2020-03-09 11:38:51 -0600

    added stying to app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <h1 className= 'App-title'>Counter Example</h1>
-        < Counter />
+        < Counter fontSize={'25px'} color={'red'}/>
+        < Counter fontSize={'20px'} color={'blue'}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -4,27 +4,35 @@ class Counter extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      counter: 0 
+      counter: 0,
+      anotherthing: 'example'
     }
   }
   inc=(event)=> {
-    // this.state.counter++
     this.setState({    // everytime this.setState is called render is called
       counter: this.state.counter + 1
     })
   }
   dec=(event)=> {
-    this.setState({
-      counter: this.state.counter - 1
-    })
+    this.setState( (state) => { // this (state) is just a placeholder. It could be named anything. were not actually calling from our state here.
+      return{
+        counter: state.counter - 1
+      }
+    }
+      // counter: this.state.counter - 1 
+      // both of these functions will work 
+    )
   }
   render() {
-
+    const { fontSize, color } = this.props
     return (
-      <div>
+      <div style={{color: color, fontSize: fontSize}}>
         <button onClick={this.dec}>-</button>
         {this.state.counter}
         <button onClick={this.inc}>+</button>
+      {this.state.anotherthing}
+        <hr/>
+        <br/>
       </div>
     )
   }
